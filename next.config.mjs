021161bdf5de1e3cb70e5ee766afdc8fb85239ed
Sámuel Fekete <feketesamu@gmail.com>
2024-02-11 22:09:07 +0100

    add custom url

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 import { withPlausibleProxy } from 'next-plausible'
 
 /** @type {import('next').NextConfig} */
-const config = withPlausibleProxy()({
+const config = withPlausibleProxy({
+  customDomain: process.env.NEXT_PUBLIC_PLAUSIBLE_URL,
+})({
   images: {
     remotePatterns: [
       { hostname: 'cdn.sanity.io' },
