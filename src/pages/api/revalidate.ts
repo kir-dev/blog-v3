f2409c39a639e328d015c8e72e19861744e4934c
Trisztán Piller <60111772+triszt4n@users.noreply.github.com>
2024-03-25 14:54:59 +0100

    Chore/deps (#37)
    * Update deps
    
    * Fix footer svg
    
    * Remove unused deps
    
    * Fix invalid DOM prop
    
    * Fix revalidation
    
    ---------
    
    Co-authored-by: Trisztán Piller <trisztan.piller@snapsoft.io>

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,26 +1,34 @@
-import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 import { NextApiRequest, NextApiResponse } from 'next'
-
-const secret = process.env.REVALIDATE_SECRET
+import { parseBody } from 'next-sanity/webhook'
+import { revalidateTag } from 'next/cache'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const signature = req.headers[SIGNATURE_HEADER_NAME] as string
-  const isValid = isValidSignature(JSON.stringify(req.body), signature, secret)
-
-  if (!isValid) {
-    res.status(401).json({ success: false, message: 'Invalid signature' })
-    return
-  }
-
   try {
-    const pathToRevalidate = req.body.slug.current
-    console.log(`===== Revalidating: ${pathToRevalidate}`)
-    await res.revalidate(pathToRevalidate)
-    return res.json({ revalidated: true })
-  } catch (err) {
-    return res.status(500).send('Error while revalidating')
+    const { body, isValidSignature } = await parseBody<{
+      _type: string
+      slug?: string | undefined
+    }>(req, process.env.REVALIDATE_SECRET)
+
+    if (!isValidSignature) {
+      return new Response('Invalid Signature', { status: 401 })
+    }
+
+    if (!body?._type) {
+      return new Response('Bad Request', { status: 400 })
+    }
+
+    revalidateTag(body._type)
+    return res.json({
+      status: 200,
+      revalidated: true,
+      now: Date.now(),
+      body,
+    })
+  } catch (error: any) {
+    console.error(error)
+    return new Response(error.message, { status: 500 })
   }
 }
