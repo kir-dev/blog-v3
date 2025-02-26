aa95160587f544e092d6c54d20c72185bd60f5f4
Trisztán Piller <60111772+triszt4n@users.noreply.github.com>
2025-02-26 10:50:17 +0100

    fix: next-ui link to next/link (#52)
    * fix links to use next/link
    
    * fix aszf logic for link generation

--- src/utils/serializers/afsz.toc.serializer.tsx
+++ src/utils/serializers/afsz.toc.serializer.tsx
@@ -1,5 +1,5 @@
-import { Link } from '@nextui-org/react'
 import { PortableTextReactComponents } from '@portabletext/react'
+import Link from 'next/link'
 import { paddingCalculator, tocSerializer } from './toc.serializer'
 
 export const afszTocSerializer = {
@@ -9,7 +9,10 @@ export const afszTocSerializer = {
       const { style, _key, children } = value
       return /^h3/.test(style ?? 'normal') ? (
         <li className={paddingCalculator(style)}>
-          <Link href={`#h${_key}`}>
+          <Link
+            href={`#h${_key}`}
+            className="relative inline-flex items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity"
+          >
             {children.map((child) => child.text).join('')}
           </Link>
         </li>
